f4e50d79eb056ff756093fe90f098c843b66b229
toni-ehmcke <t.ehmcke@web.de>
2016-03-22 16:50:32 +0100

    Finished 2D-calculation and plotted n(T_e). TODO: n(kx,ky)

--- Numerik/01_1D_latice/1D_latice.py
+++ Numerik/01_1D_latice/1D_latice.py
@@ -36,11 +36,11 @@ def get_R_e(E, M, g_e, T_e):
     # transform energy-vector into matrices
     mat_E_x, mat_E_y = np.meshgrid(E,E) 
     mat_diff = mat_E_y - mat_E_x        # matrix representing: E_i - E_j
-    R_e = np.ones((M,M))*g_e**2 * T_e/4 # matrix for transition rates
+    R_e = np.ones((M,M))*g_e**2 * T_e # matrix for transition rates
     ind = np.abs(mat_diff) > 0          # indices of the non-divergent elements
     # fill in just those elements without divergences 1/0
     # the rest is set to the correct limit
-    R_e[ind] = g_e**2 * mat_diff[ind]/(4*np.exp(mat_diff[ind]/T_e)-4)
+    R_e[ind] = g_e**2 * mat_diff[ind]/(np.exp(mat_diff[ind]/T_e)-1)
     return R_e
 
 def get_R_e_test(E, M, g_e, T_e, R_e, epsilon):
@@ -50,10 +50,10 @@ def get_R_e_test(E, M, g_e, T_e, R_e, epsilon):
     for i in range(M):
         for j in range(M):
             if i != j:
-                R_e_test[i,j] = g_e**2*(E[i] - E[j])/(4*np.exp((E[i] 
-                                                        - E[j])/T_e)-4)
+                R_e_test[i,j] = g_e**2*(E[i] - E[j])/(np.exp((E[i] 
+                                                        - E[j])/T_e)-1)
             else:
-                R_e_test[i,j] = g_e**2 * T_e/4
+                R_e_test[i,j] = g_e**2 * T_e
     return np.abs(R_e_test - R_e) < epsilon
 
 def get_R_h(E, M, l, g_h, T_h):
@@ -63,11 +63,11 @@ def get_R_h(E, M, l, g_h, T_h):
     mat_diff = mat_E_y - mat_E_x        # matrix representing: E_i - E_j
         
     # leave out the sine-terms at first
-    R_h = np.ones((M,M))*g_h**2 * T_h   # matrix for transition rates
+    R_h = np.ones((M,M))*g_h**2 * T_h*4   # matrix for transition rates
     ind = np.abs(mat_diff) > 0          # indices of the non-divergent elements
     # fill in just those elements without divergences 1/0
     # the rest is set to the correct limit
-    R_h[ind] = g_h**2 * mat_diff[ind]/(np.exp(mat_diff[ind]/T_h)-1)
+    R_h[ind] = g_h**2 *4* mat_diff[ind]/(np.exp(mat_diff[ind]/T_h)-1)
     
     # multiply the sine-terms
     sin = np.sin(l*np.arange(1,M+1)*np.pi/(M+1))**2 # vector with sine-values sin(li)**2
@@ -85,10 +85,10 @@ def get_R_h_test(E, M, l, g_h, T_h, R_h, epsilon):
     for i in range(M):
         for j in range(M):
             if i != j:
-                R_h_test[i,j] = g_h**2*(E[i] - E[j]) * sin[i] * sin[j]\
+                R_h_test[i,j] = g_h**2* 4* (E[i] - E[j]) * sin[i] * sin[j]\
                             /(np.exp((E[i] - E[j])/T_h)-1)
             else:
-                R_h_test[i,j] = g_h**2 * T_h * sin[i] * sin[j]
+                R_h_test[i,j] = g_h**2 *4* T_h * sin[i] * sin[j]
     return np.abs(R_h_test - R_h) < epsilon
 
 def get_R(R_e,R_h):
@@ -261,11 +261,11 @@ print __doc__
 # ------------------------------set parameters-----------------------------
 #---------------------------physical parameters--------------------------------
 J = 1.                              # dispersion-constant
-M = 150                             # system size (number of sites)
+M = 300                             # system size (number of sites)
 n = 3                               # density
 N = n*M                             # number of particles
-l = 4                               # heated site
-g_h = 0.                            # coupling strength needle<->system
+l = 4.                               # heated site
+g_h = 1.                            # coupling strength needle<->system
 g_e = 1.                            # coupling strength environment<->sys
 T_h = 60 * J                        # temperature of the needle
 
--- Numerik/02_2D_lattice/2D_lattice.py
+++ Numerik/02_2D_lattice/2D_lattice.py
@@ -33,10 +33,10 @@ def get_vec_k(kx, ky, Mx, My):
             k[i*Mx + j] = (kx[j],ky[i])
     return k 
     
-def get_E(k, Jx, Jy, Mx, My):
+def get_E(k, Jx, Jy, M):
     """ Return a vector with the energy E at given quasimomenta kx, ky with the 
     dispersion relation."""
-    E = np.zeros(Mx*My)
+    E = np.zeros(M)
     E = -2*(Jx*np.cos(k['kx'])+Jy*np.cos(k['ky']))
     return E
     
@@ -71,39 +71,39 @@ def getIndexVec(a, i, m, n, retMat=False):
     else:
         print "Vector and matrix must have the same dimensions!"
     
-def get_R_e(E, Mx, My, g_e, T_e):
+def get_R_e(E, M, g_e, T_e):
     """ Return the contribution of the environment to the transition rate."""
     # transform energy-vector into matrices
     mat_E_x, mat_E_y = np.meshgrid(E,E) 
     mat_diff = mat_E_y - mat_E_x        # matrix representing: E_i - E_j
     # matrix for transition rates
-    R_e = np.ones((Mx*My,Mx*My))* g_e**2 * T_e/Mx/My 
-    ind = np.abs(mat_diff) > 0          # indices of the non-divergent elements
+    R_e = np.ones((M,M))* g_e**2 * T_e 
+    ind = np.abs(mat_diff) > 10e-10          # indices of the non-divergent elements
     # fill in just those elements without divergences 1/0
     # the rest is set to the correct limit
-    R_e[ind] = g_e**2 * mat_diff[ind]/(np.exp(mat_diff[ind]/T_e)-1)/Mx/My
+    R_e[ind] = g_e**2 * mat_diff[ind]/(np.exp(mat_diff[ind]/T_e)-1)
     return R_e
 
-def get_R_e_test(E, Mx, My, g_e, T_e, R_e, epsilon):
+def get_R_e_test(E, M, g_e, T_e, R_e, epsilon):
     """ A test routine for checking whether the calculation in get_R_e is 
     correct. Epsilon is the maximal accepted deviation between the results."""
-    R_e_test = np.zeros((Mx*My,Mx*My))
-    for i in range(Mx*My):
-        for j in range(Mx*My):
+    R_e_test = np.zeros((M,M))
+    for i in range(M):
+        for j in range(M):
             if np.abs(E[i]-E[j]) > 0:
                 R_e_test[i,j] = g_e**2*(E[i] - E[j])/(np.exp((E[i] 
-                                                        - E[j])/T_e)-1)/Mx/My
+                                                        - E[j])/T_e)-1)
             else:
-                R_e_test[i,j] = g_e**2 * T_e/Mx/My
+                R_e_test[i,j] = g_e**2 * T_e
     return np.all(np.abs(R_e_test - R_e) < epsilon)
 
-def get_vec_sin(k, Mx, My, lx, ly):
+def get_vec_sin(k, M, lx, ly):
     """ Return a vector of all possible sin**2(kx lx)*sin**2(ky*ly) terms."""
-    sin = np.zeros(Mx*My)
+    sin = np.zeros(M)
     sin = np.sin(k['kx']*lx)**2 * np.sin(k['ky']*ly)**2
     return sin
 
-def get_R_h(E, Mx, My, lx, ly, kx, ky, k, g_h, T_h):
+def get_R_h(E, M, lx, ly, kx, ky, k, g_h, T_h):
     """ Return the contribution of the hot needle to the transition rate."""
     # transform energy-vector into matrices
     mat_E_x, mat_E_y = np.meshgrid(E,E)    
@@ -111,33 +111,19 @@ def get_R_h(E, Mx, My, lx, ly, kx, ky, k, g_h, T_h):
         
     # leave out the sine-terms at first
     # matrix for transition rates
-    R_h = np.ones((Mx*My,Mx*My))*g_h**2 * T_h * 16/Mx/My 
-    ind = np.abs(mat_diff) > 0          # indices of the non-divergent elements
+    R_h = np.ones((M,M))*g_h**2 * T_h * 16 
+    ind = np.abs(mat_diff) > 10e-10          # indices of the non-divergent elements
     # fill in just those elements without divergences 1/0
     # the rest is set to the correct limit
-    R_h[ind] = g_h**2 * mat_diff[ind]/(np.exp(mat_diff[ind]/T_h)-1)
+    R_h[ind] = g_h**2 *16* mat_diff[ind]/(np.exp(mat_diff[ind]/T_h)-1)
     
     # multiply the sine-terms
-    vec_sin = get_vec_sin(k, Mx, My, lx, ly) 
+    vec_sin = get_vec_sin(k, M, lx, ly) 
     # transform sine-vectors into matrices
     mat_sin_x, mat_sin_y = np.meshgrid(vec_sin, vec_sin)
     R_h *= mat_sin_x * mat_sin_y
     
     return R_h
-
-def get_R_h_test(E, M, l, g_h, T_h, R_h, epsilon):
-    """ A test routine for checking whether the calculation in get_R_h is 
-    correct. Epsilon is the maximal accepted deviation between the results."""
-    R_h_test = np.zeros((M,M))
-    sin = np.sin(l*np.arange(1,M+1)*np.pi/(M+1))**2
-    for i in range(M):
-        for j in range(M):
-            if i != j:
-                R_h_test[i,j] = g_h**2*(E[i] - E[j]) * sin[i] * sin[j]\
-                            /(np.exp((E[i] - E[j])/T_h)-1)
-            else:
-                R_h_test[i,j] = g_h**2 * T_h * sin[i] * sin[j]
-    return np.abs(R_h_test - R_h) < epsilon
 
 def get_R(R_e,R_h):
     """ Calculate the transition rate matrix. The element (i,j) accords to
@@ -192,7 +178,8 @@ def get_mat_n(T_e, r_0, M, N_T, N, E, g_e, R_h, tmpN_t, tmpN_max):
             n = np.zeros(M)                 # vector of all occupation numbers
             n[0], n[1:] = n1 , solution[0] 
             if np.any(n<0.):                # if solution is unphysical      
-                print "Needed to repeat calculation at Temperature T_e =", T_e[-i-1] 
+                print "Needed to repeat calculation at Temperature T_e =", T_e[-i-1],\
+                        "with index i = ", i
                 n = get_cor_n(i, T_e, r_0, M, N, E, g_e, R_h, tmpN_t, tmpN_max)
                 if n == None:
                     print "Calculation failed! You may choose a larger tmpN_max."
@@ -227,27 +214,36 @@ def get_cor_n(i, T_e, r_0, M, N, E, g_e, R_h, tmpN_t, tmpN_max):
         tmpT = get_tmpT(T_e, i, tmpN_t)         # reduced temperature array
                                                 # for closer sample points    
         for j in range(tmpN_t):
-            R_e = get_R_e(E, M, g_e, tmpT[-j-1])# matrix with transition rates (env)
+            R_e = get_R_e(E, M, g_e,       # rate-matrix of the environment
+                          tmpT[-j-1])  
             #print get_R_e_test(E, M, g_e, tmpT, R_e, 10e-15)
             R = get_R(R_e, R_h)                 # total transition rates
             data = (R, M, N)                    # arguments for fsolve  
             #-----------solve the nonlinear system of equations--------------------    
             solution = fsolve(func, r_0,args=data, full_output=1)
             if solution[2] == 0:                # if sol. didnt conv., increase s.p.
-                tmpN_t *= 10
+                tmpN_t *= 4
+                print "Increased tmpN because of a nonconvergent solution!"
                 break
             else:
                 n1 = get_n1(solution[0],N) # occupation number of the ground state
                 n = np.zeros(M)            # vector of all occupation numbers
                 n[0], n[1:] = n1 , solution[0] 
                 if np.any(n<0.):           # if solution is unphysical        
-                    tmpN_t *= 2           # increase num of sample points
+                    tmpN_t *= 4           # increase num of sample points
+                    print "Increased tmpN because of an unphysical solution!"
                     break
                 elif j!= tmpN_t-1:         # if iteration is not finished
                     r_0 = solution[0]      # just change initial guess 
                 else:
+                    print "Found a better solution! :)"
                     return n
-        print "Increased tmpN!"
+        
+        
+def plot_axT(T_e, M, mat_n):
+    """ Plots the occupation numbers for different environment temperatures."""
+    for i in range(M):
+        axT.plot(T_e,np.abs(mat_n[i,:]), c = 'b')  
         
 
 #def main():
@@ -257,52 +253,58 @@ print __doc__
 #---------------------------physical parameters--------------------------------
 Jx = 1.                             # dispersion-constant in x-direction
 Jy = 1.                             # dispersion-constant in y-direction   
-Mx = 5                            # system size in x-direction
-My = 2                              # system size in y-direction
-lx = 4                              # heated site (x-component)
-ly = 1                              # heated site (y-component)
+Mx = 30                            # system size in x-direction
+My = 10                              # system size in y-direction
+lx = 6.                              # heated site (x-component)
+ly = 5.                              # heated site (y-component)
 n = 3                               # particle density
 g_h = 1.                            # coupling strength needle<->system
 g_e = 1.                            # coupling strength environment<->sys
-T_h = 60 * Jx                        # temperature of the needle
-N = n*Mx*My                         # number of particles
-
-
+T_h = 60*Jx                        # temperature of the needle
+M = Mx * My                         # new 2D-system size
+N = n*M                         # number of particles
 
 #----------------------------program parameters--------------------------------
-N_T = 100                           # number of temp. data-points
+N_T = 150                           # number of temp. data-points
 tmpN_t = 4                          # number of temp. data-points in
                                     # temporary calculations
 epsilon = 10e-10                    # minimal accuracy for the compare-test
 tmpN_max = 256                      # maximal number of subslices
-T_e = 1.#np.logspace(-2,2,N_T)         # temperatures of the environment 
- 
-
+T_e = np.logspace(-2,2,N_T)         # temperatures of the environment 
     
 #--------------calculate environment temp. independent parameters----------
 kx = get_k(Mx)                      # vector of all quasimomenta in x-dir
 ky = get_k(My)                      # vector of all quasimomenta in y-dir
-print kx
-print ky
 
 k = get_vec_k(kx, ky, Mx, My)       # vector of tuples of (kx,ky)
-print k
 
-E = get_E(k, Jx, Jy, Mx, My)        # vector of all energyeigenvalues
-print E
+E = get_E(k, Jx, Jy, M)        # vector of all energyeigenvalues
 
-R_e = get_R_e(E, Mx, My, g_e, T_e)  # rate-matrix of the environment
-#print get_R_e_test(E, Mx, My, g_e, T_e, R_e, epsilon) 
-R_h = get_R_h(E, Mx, My, lx, ly, kx, ky, k, g_h, T_h)    # matrix with transition rates (needle)
-print np.shape(R_h)
-#print R_h
-# print np.all(get_R_h_test(E, M, l, g_h, T_h, R_h, 10e-10))
+
+#print get_R_e_test(E, M, g_e, T_e, R_e, epsilon) 
+R_h = get_R_h(E, M, lx, ly, kx, ky, k, g_h, T_h)    # matrix with transition rates (needle)
+
     
 #-----calculate the occupation numbers in dependency of the temp-----------
-#r_0 = n * np.ones(M-1)              # initial guess for n2,...,n_m
-#mat_n = get_mat_n(T_e, r_0, M, N_T, N, E, g_e, R_h, tmpN_t, tmpN_max)            
+r_0 = n * np.ones((M)-1)              # initial guess for n2,...,n_m
+mat_n = get_mat_n(T_e, r_0, M, N_T, N, E, g_e, R_h, tmpN_t, tmpN_max) 
 
+fig = plt.figure("Mean-field occupation", figsize=(16,9))
+axT = fig.add_subplot(111)
+axT.set_xlabel(r'$T/J$')
+axT.set_ylabel(r'$\bar{n}_i$')
+axT.set_xlim([np.min(T_e), np.max(T_e)])
+axT.set_ylim([8*10e-5, 3*N])
+axT.set_xscale('log')
+axT.set_yscale('log')
 
+plot_axT(T_e, M, mat_n)
+
+# refreshing
+fig.canvas.draw()
+    
+matplotlib.rcParams.update({'font.size': 18})
+plt.show()
               
 #if __name__ == '__main__':
 #   main()
